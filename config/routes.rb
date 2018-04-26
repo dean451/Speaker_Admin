8de95ec23460bc64edaae42f2cca831b32e22729
Dean Palm <dean@sxsw.com>
2018-04-26 15:28:08 -0500

    routes clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :users
-  get 'users/new'
-
   root 'static_pages#sessions'
 
   get '/sessions', to: 'static_pages#sessions'
@@ -10,7 +7,14 @@ Rails.application.routes.draw do
   get '/staff', to: 'static_pages#staff'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+
   get  '/signup',  to: 'users#new'
-  post '/signup',  to: 'users#create'
+  post '/signup', to: 'users#create'
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
 
 end
